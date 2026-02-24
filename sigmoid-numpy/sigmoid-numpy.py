b4adf457cd990c7851924d73337f065f40c1c409
icyalmond <vugiabao1507@gmail.com>
2026-02-24 23:12:57 +0700

    Update solution for sigmoid-numpy

--- sigmoid-numpy/sigmoid-numpy.py
+++ sigmoid-numpy/sigmoid-numpy.py
@@ -4,7 +4,7 @@ def sigmoid(x):
     """
     Vectorized sigmoid function.
     """
-    a = np.array(x, dtype = float)
+    a = np.asarray(x, dtype = float)
     a = 1 / (1 + np.exp(-a))
     return a
     pass
