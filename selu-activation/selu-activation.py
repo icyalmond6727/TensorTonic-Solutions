1481513e1013f5fe36359e9ebd3084593ae58b93
icyalmond <vugiabao1507@gmail.com>
2026-03-01 23:05:55 +0700

    Update solution for selu-activation

--- selu-activation/selu-activation.py
+++ selu-activation/selu-activation.py
@@ -11,6 +11,6 @@ def selu(x, lam=1.0507009873554804934193349852946, alpha=1.673263242354377284817
     Apply SELU activation element-wise.
     Returns a list of floats rounded to 4 decimal places.
     """
-    a = np.asarray(x, dtype='float')
+    a = np.asarray(x, dtype = float)
     a = np.vectorize(calc)(a, lam, alpha)
     return a
