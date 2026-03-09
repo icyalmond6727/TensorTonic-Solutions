63769f5be76f5fa527fd49a6ef654c12bebdbe77
icyalmond <vugiabao1507@gmail.com>
2026-03-09 23:30:21 +0700

    Update solution for linear-regression-closed-form

--- linear-regression-closed-form/linear-regression-closed-form.py
+++ linear-regression-closed-form/linear-regression-closed-form.py
@@ -9,6 +9,8 @@ def linear_regression_closed_form(X, y):
     => X^T * X * w = X^T * y
     => w = (X^T * X)^(-1) * X^T * y
     """
+    X = np.asarray(X)
+    y = np.asarray(y)
     XT = np.linalg.matrix_transpose(X)
     return np.linalg.inv(XT @ X) @ XT @ y
     
