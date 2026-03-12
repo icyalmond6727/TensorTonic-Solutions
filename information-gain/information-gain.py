dcaebe1ebf5cd4f1374ce08ed5bcd25fa3341c9a
icyalmond <vugiabao1507@gmail.com>
2026-03-12 20:56:16 +0700

    Add solution for information-gain

---
+++ information-gain/information-gain.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def _entropy(y):
+    """
+    Helper: Compute Shannon entropy (base 2) for labels y.
+    """
+    labels, counts = np.unique(y, return_counts = True)
+    counts = counts[counts > 0]
+    p = counts / len(y)
+    p = -(p * np.log2(p))
+    return p.sum()
+
+def information_gain(y, split_mask):
+    """
+    Compute Information Gain of a binary split on labels y.
+    Use the _entropy() helper above.
+    """
+    left_y = y[split_mask]
+    right_y = y[~split_mask]
+    return _entropy(y) - (len(left_y) / len(y) * _entropy(left_y) + len(right_y) / len(y) * _entropy(right_y))
