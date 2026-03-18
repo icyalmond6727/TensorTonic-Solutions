99e6ac58d83a4320eaca2b993918ad7f0c7d26b0
icyalmond <vugiabao1507@gmail.com>
2026-03-18 20:20:38 +0700

    Update solution for gini-impurity

--- gini-impurity/gini-impurity.py
+++ gini-impurity/gini-impurity.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 def giny(y):
+    if len(y) == 0: return float(0)
     labels, counts = np.unique(y, return_counts = True)
     counts = counts[counts > 0]
-    if len(y) == 0: return float(0)
     p = counts / len(y)
     p = p ** 2
     return 1 - p.sum()
@@ -12,9 +12,9 @@ def gini_impurity(y_left, y_right):
     """
     Compute weighted Gini impurity for a binary split.
     """
+    if len(y_left) == 0 and len(y_right) == 0: return float(0)
     y_left = np.asarray(y_left)
     y_right = np.asarray(y_right)
-    if len(y_left) == 0 and len(y_right) == 0: return float(0)
     p_left = len(y_left) / (len(y_left) + len(y_right))
     p_right = 1 - p_left
     return giny(y_left) * p_left + giny(y_right) * p_right
