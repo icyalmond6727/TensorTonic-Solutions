c5c1d66487d3708615bee0462a81ae82a74e6d77
icyalmond <vugiabao1507@gmail.com>
2026-03-18 20:22:18 +0700

    Update solution for entropy-node

--- entropy-node/entropy-node.py
+++ entropy-node/entropy-node.py
@@ -4,6 +4,7 @@ def entropy_node(y):
     """
     Compute entropy for a single node using stable logarithms.
     """
+    if len(y) == 0: return float(0)
     labels, counts = np.unique(y, return_counts = True)
     counts = counts[counts > 0]
     p = counts / len(y)
